7f9aea13deecc4d8a195b86896170aaaa5b0015c
Ynln69 <106525821+Ynln69@users.noreply.github.com>
2024-09-30 19:36:51 +0000

    Deploying to gh-pages from @ Ynln69/lego-project@283afbac412f2d08531d0c9515a8dd579915f19e 🚀

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const c of t)if(c.type==="childList")for(const r of c.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function e(t){const c={};return t.integrity&&(c.integrity=t.integrity),t.referrerPolicy&&(c.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?c.credentials="include":t.crossOrigin==="anonymous"?c.credentials="omit":c.credentials="same-origin",c}function i(t){if(t.ep)return;t.ep=!0;const c=e(t);fetch(t.href,c)}})();fetch("./data/products-data.json").then(s=>s.json()).then(s=>{const o=document.getElementById("our-products");s.products.forEach(e=>{const i=document.createElement("li");i.classList.add("our-products-item","products-home-item"),i.innerHTML=`
+        <a href="../pages/product-page.html?id=${e.id}">
+        <img src="${e.photo}" alt="${e.title}" class="products-item-img" >
+        <div class="products-item-thumb">
+          <h3 class="products-item-title">${e.title}</h3>
+          <p class="products-item-desk">${e.description}</p>
+          <button type="button" class="basket-btn">${e.button}
+            <svg width="16" height="16">
+                <use href="../img/icon/icon-defs.svg#icon-shopping-cart"></use>
+            </svg>
+        </button>
+        </div>
+        </a>
+      `,o.appendChild(i)})}).catch(s=>console.error("Помилка при завантаженні JSON:",s));fetch("./data/certificates-data.json").then(s=>s.json()).then(s=>{const o=document.getElementById("certificates-list");s.certificates.forEach(e=>{const i=document.createElement("li");i.classList.add("certificates-item"),i.classList.add("certificates-home-item"),i.innerHTML=`
+      <h3 class="certificates-item-title">${e.title}</h3>
+        <div class="certificates-item-thumb">
+          <p class="certificates-item-price">${e.price}</p>
+          <p class="certificates-item-desk">${e.description}</p>
+        </div>
+        <button type="button" class="basket-btn">${e.button}
+            <svg width="16" height="16">
+                <use href="../img/icon/icon-defs.svg#icon-shopping-cart"></use>
+            </svg>
+        </button>
+      `,o.appendChild(i)})}).catch(s=>console.error("Помилка при завантаженні JSON:",s));
 //# sourceMappingURL=index.js.map
