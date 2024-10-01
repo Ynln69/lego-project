fde6c2d2488e69153201b2d7afa86735cd02778b
Ynln69 <106525821+Ynln69@users.noreply.github.com>
2024-10-01 21:42:53 +0000

    Deploying to gh-pages from @ Ynln69/lego-project@0836d616b465f15d02b486378e851a4a7ceea9b6 🚀

--- product-page.js
+++ product-page.js
@@ -1,2 +1,16 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const n=new URLSearchParams(window.location.search),c=n.get("id");fetch("../../data/products-data.json").then(t=>t.json()).then(t=>{const e=t.products.find(o=>o.id==c);e?(document.getElementById("product-image").src=e.photo,document.getElementById("product-price").textContent=`${e.price} PLN`,document.getElementById("product-title").textContent=e.title,document.getElementById("product-description").textContent=e.description):console.error("Продукт не знайдено")}).catch(t=>console.error("Помилка при завантаженні JSON:",t));
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{a as e}from"./assets/cart-count-Bzsmujp2.js";const n=new URLSearchParams(window.location.search),r=n.get("id");fetch("../../data/products-data.json").then(o=>o.json()).then(o=>{const t=o.products.find(c=>c.id==r);if(t){const c=`
+          <div>
+            <img src="${t.photo}" alt="${t.title}" class="product-image" />
+          </div>
+          <div class="product-info">
+            <h2 class="product-info-title">${t.title}</h2>
+            <p class="product-info-price">${t.price} PLN</p>
+            <p class="product-info-description">${t.description}</p>
+            <button type="button" class="basket-btn" data-id="${t.id}">${t.button}
+              <svg width="16" height="16">
+                  <use href="../img/icon/icon-defs.svg#icon-shopping-cart"></use>
+              </svg>
+            </button>
+          </div>
+      `;document.getElementById("product-container").innerHTML=c,document.querySelector(".basket-btn").addEventListener("click",()=>{e(t)})}else console.error("Продукт не знайдено")}).catch(o=>console.error("Помилка при завантаженні JSON:",o));
 //# sourceMappingURL=product-page.js.map
